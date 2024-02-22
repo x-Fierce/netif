b43b690efe98a2da94b60e70297f2d43507e9f4e
Fierce <x-qu@163.com>
2024-02-22 12:14:29 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/x-Fierce/netif
+
+go 1.18
+
+require (
+	github.com/n-marshall/fn v0.0.0-20170411202714-726fd988d1cf
+	github.com/n-marshall/go-cp v0.0.0-20180115193924-61436d3b7cfa
+)
---
+++ interfaces.go
@@ -0,0 +1,26 @@
+package netif
+
+import "github.com/n-marshall/fn"
+
+type InterfaceSet struct {
+	InterfacesReader
+
+	InterfacesPath string
+	Adapters       []*NetworkAdapter
+}
+
+func NewInterfaceSet(opts ...fn.Option) *InterfaceSet {
+	fnConfig := fn.MakeConfig(
+		fn.Defaults{"path": "/etc/network/interfaces"},
+		opts,
+	)
+	path := fnConfig.GetString("path")
+
+	return &InterfaceSet{
+		InterfacesPath: path,
+	}
+}
+
+func Path(path string) fn.Option {
+	return fn.MakeOption("path", path)
+}
